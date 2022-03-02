355054179e331990cf944ad41da30701b5cf741e
Paul Reece <paulreece2@gmail.com>
2022-03-01 22:05:46 -0500

    I added restart to Dealer bust, Dealer win, Player win, Player bust, Player/Dealer tie.  I added colors to the Dealer and player to make it easier to keep track of.  I added colors to the cards depending on their suit.  Added Dealer score to print above player score every time.  Added a requirement to type Hit or Stay as the player, if you don't it will keep asking you to try again.  Lastly I added much more logic and math to account for a player/dealer tie.  SO MUCH FUNNNNNN

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,10 @@
 import random
+from click import style
+from colorama import init
+from colorama import Fore, Back, Style
+import os
+import sys
+init()
 
 
 class Card(object):
@@ -30,7 +36,8 @@ class Card(object):
 
 
 class Deck(object):
-    suits = ["Diamonds", "Spades", "Hearts", "Clubs"]
+    suits = [Fore.WHITE + Style.BRIGHT + Back.RED +
+             "Diamonds", Fore.BLACK + Back.WHITE + Style.NORMAL + "Spades", Fore.WHITE + Style.BRIGHT + Back.RED + "Hearts", Fore.BLACK + Back.WHITE + Style.NORMAL + "Clubs"]
     values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
     def __init__(self):
@@ -68,7 +75,8 @@ class Player(object):
 
     def show_hand(self):
         for card in self.hand:
-            print("Player Card: " + card.show_card())
+            print(Fore.WHITE + Style.BRIGHT + Back.MAGENTA +
+                  "Player Card: " + card.show_card())
 
     def evaluate_hand_points(self):
         points = 0
@@ -88,7 +96,8 @@ class Dealer(object):
 
     def show_hand(self):
         for card in self.hand:
-            print("Dealer card:" + card.show_card())
+            print(Fore.WHITE + Style.BRIGHT + Back.BLUE +
+                  "Dealer card:" + card.show_card())
 
     def evaluate_hand_points(self):
         points = 0
@@ -97,32 +106,52 @@ class Dealer(object):
         return points
 
 
+def restart():
+    restart = input("\nDo you want to restart the Game? [y/n] > ")
+    if restart == "y":
+        os.execl(sys.executable, sys.executable,
+                 os.path.abspath(__file__), *sys.argv)
+    else:
+        print("\nGoodbye, thanks for playing!")
+        sys.exit(0)
+
+
 f_deck = Deck()
 
 dealer = Dealer("dealer")
 dealer.draw(f_deck).draw(f_deck)
-total = dealer.evaluate_hand_points()
-print("Dealer total:", total)
 
 
 def totaled():
-    if total < 17:
-        global totallied
-        global totalotaled
+    global total
+    total = dealer.evaluate_hand_points()
+    if total > 21:
+        print("Dealer Bust!  You win!!!")
+        restart()
+    elif total >= 17:
+        print(Fore.WHITE + Style.BRIGHT +
+              Back.BLUE + "Dealer total:", str(total))
+    elif total < 17:
         dealer.draw(f_deck)
-        totallied = dealer.evaluate_hand_points()
-        print("Dealer total:", str(totallied))
-        if totallied < 17:
+        total = dealer.evaluate_hand_points()
+        print(Fore.WHITE + Style.BRIGHT + Back.BLUE +
+              "Dealer total:", str(total))
+        if total > 21:
+            print("Dealer Bust!  You win!!!")
+            restart()
+        elif total < 17:
             dealer.draw(f_deck)
-            totalotaled = dealer.evaluate_hand_points()
-            print("Dealer total:", str(totalotaled))
-            if totalotaled < 17:
+            total = dealer.evaluate_hand_points()
+            print(Fore.WHITE + Style.BRIGHT + Back.BLUE +
+                  "Dealer total:", str(total))
+            if total > 21:
+                print("Dealer Bust!  You win!!!")
+                restart()
+            elif total < 17:
                 dealer.draw(f_deck)
-                totalized = dealer.evaluate_hand_points()
-                print("Dealer total:", str(totalized))
-
-
-totaled()
+                total = dealer.evaluate_hand_points()
+                print(Fore.WHITE + Style.BRIGHT + Back.BLUE +
+                      "Dealer total:", str(total))
 
 
 dealer.show_hand()
@@ -130,67 +159,161 @@ player = Player("player")
 player.draw(f_deck)
 player.show_hand()
 playtotal = player.evaluate_hand_points()
-print("Player total: ", playtotal)
-hit = input("Hit or Stay?")
+
+theTotal = totaled()
+print(Fore.WHITE + Style.BRIGHT + Back.MAGENTA + "Player total:", playtotal)
 
 
 def hitme():
+    action = ["Hit", "Stay"]
+    hit = input(Fore.WHITE + Style.BRIGHT + Back.GREEN + "Hit or Stay? ")
+    while hit not in action:
+        if hit not in action:
+            print("Please type Hit or Stay.")
+            hit = input(Fore.WHITE + Style.BRIGHT +
+                        Back.GREEN + "Hit or Stay? ")
+        elif hit in action:
+            break
     if hit == "Hit":
         player.draw(f_deck)
         player.show_hand()
         playtotaled = player.evaluate_hand_points()
-        print("Player total: ", playtotaled)
-        if playtotaled > 21:
-            print("Bust! You lose, better luck next time!")
+        print(Fore.WHITE + Style.BRIGHT + Back.BLUE + "Dealer total:", total)
+        print(Fore.WHITE + Style.BRIGHT + Back.MAGENTA +
+              "Player total:", playtotaled)
+        if 21 - playtotaled < 21 - total:
+            print("You beat the computer, good job!!")
+            restart()
+        elif playtotaled == total:
+            print("Draw!!!")
+            restart()
         elif playtotaled == 21:
             print("You win!!")
+            restart()
         elif playtotaled < 21:
-            hitter = input("Hit or Stay?")
+            hitter = input(Fore.WHITE + Style.BRIGHT +
+                           Back.GREEN + "Hit or Stay? ")
+            while hitter not in action:
+                if hitter not in action:
+                    print("Please type Hit or Stay.")
+                    hitter = input(Fore.WHITE + Style.BRIGHT +
+                                   Back.GREEN + "Hit or Stay? ")
+                elif hitter in action:
+                    break
             if hitter == "Stay":
                 if 21 - playtotaled < 21 - total:
                     print("You beat the computer, good job!!")
+                    restart()
+                elif playtotaled == total:
+                    print("Draw!!!")
+                    restart()
+                elif playtotaled == 21:
+                    print("You win!!")
+                    restart()
                 elif 21 - playtotaled > 21 - total:
-                    print("Sorry the computer beat you this time!")
+                    print("Bust!! Sorry the computer beat you this time!")
+                    restart()
             elif hitter == "Hit":
                 player.draw(f_deck)
                 player.show_hand()
                 playaplaya = player.evaluate_hand_points()
-                print("Player total: ", playaplaya)
+                print(Fore.WHITE + Style.BRIGHT +
+                      Back.BLUE + "Dealer total:", total)
+                print(Fore.WHITE + Style.BRIGHT +
+                      Back.MAGENTA + "Player total:", playaplaya)
                 if playaplaya > 21:
                     print("Bust! You lose, better luck next time!")
+                    restart()
+                elif playaplaya == total:
+                    print("Draw!!!")
+                    restart()
                 elif playaplaya == 21:
                     print("You win!!")
+                    restart()
                 elif playaplaya < 21:
-                    hittery = input("Hit or Stay?")
+                    hittery = input(Fore.WHITE + Style.BRIGHT +
+                                    Back.GREEN + "Hit or Stay? ")
+                    while hittery not in action:
+                        if hittery not in action:
+                            print("Please type Hit or Stay.")
+                            hittery = input(Fore.WHITE + Style.BRIGHT +
+                                            Back.GREEN + "Hit or Stay? ")
+                        elif hittery in action:
+                            break
                     if hittery == "Stay":
-                        if 21 - playtotaled < 21 - total:
+                        if 21 - playaplaya < 21 - total:
                             print("You beat the computer, good job!!")
-                        elif 21 - playtotaled > 21 - total:
-                            print("Sorry the computer beat you this time!")
+                            restart()
+                        elif playaplaya == total:
+                            print("Draw!!!")
+                            restart()
+                        elif playaplaya == 21:
+                            print("You win!!")
+                            restart()
+                        elif 21 - playaplaya > 21 - total:
+                            print("Bust!! Sorry the computer beat you this time!")
+                            restart()
                     elif hittery == "Hit":
                         player.draw(f_deck)
                         player.show_hand()
                         playaplayit = player.evaluate_hand_points()
-                        print("Player total: ", playaplayit)
+                        print(Fore.WHITE + Style.BRIGHT +
+                              Back.BLUE + "Dealer total:", total)
+                        print(Fore.WHITE + Style.BRIGHT +
+                              Back.MAGENTA + "Player total:", playaplayit)
                         if playaplayit > 21:
                             print("Bust! You lose, better luck next time!")
+                            restart()
                         elif playaplayit == 21:
                             print("You win!!")
+                            restart()
                         if playaplayit < 21:
-                            hitityo = input("Hit or Stay?")
+                            hitityo = input(
+                                Fore.WHITE + Style.BRIGHT + Back.GREEN + "Hit or Stay?")
+                            while hitityo not in action:
+                                if hitityo not in action:
+                                    print("Please type Hit or Stay.")
+                                    hitityo = input(Fore.WHITE + Style.BRIGHT +
+                                                    Back.GREEN + "Hit or Stay? ")
+                                elif hitityo in action:
+                                    break
                             if hitter == "Stay":
-                                if 21 - playtotaled < 21 - total:
+                                if 21 - playaplayit < 21 - total:
                                     print("You beat the computer, good job!!")
-                                elif 21 - playtotaled > 21 - total:
+                                    restart()
+                                elif playaplayit == total:
+                                    print("Draw!!!")
+                                    restart()
+                                elif playaplayit == 21:
+                                    print("You win!!")
+                                    restart()
+                                elif 21 - playaplayit > 21 - total:
                                     print(
-                                        "Sorry the computer beat you this time!")
+                                        "Bust!! Sorry the computer beat you this time!")
+                                    restart()
                             if hitityo == "Hit":
                                 player.draw(f_deck)
                                 player.show_hand()
                                 playaplayedd = player.evaluate_hand_points()
-                                print("Player total: ", playaplayedd)
-                # elif playaplaya > 21:
-                #     print("You lose!!")
+                                print(Fore.WHITE + Style.BRIGHT +
+                                      Back.BLUE + "Dealer total:", total)
+                                print(Fore.WHITE + Style.BRIGHT +
+                                      Back.MAGENTA + "Player total:", playaplayedd)
+                                if 21 - playaplayedd < 21 - total:
+                                    print("You beat the computer, good job!!")
+                                    restart()
+                                elif playaplayedd == total:
+                                    print("Draw!!!")
+                                    restart()
+                                elif playaplayedd == 21:
+                                    print("You win!!")
+                                    restart()
+                                elif 21 - playaplayedd > 21 - total:
+                                    print(
+                                        "Bust!! Sorry the computer beat you this time!")
+                                    restart()
+                                    # elif playaplaya > 21:
+                                    #     print("You lose!!")
 
 
 hitme()
